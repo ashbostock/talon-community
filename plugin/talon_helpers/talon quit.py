bdf16572355e29ad3ce891cb5b9493ccbe9b02d8
Ash Bostock <abostock@gmail.com>
2025-08-28 12:51:35 +0100

    add commands to allow you to quit and restart talon

---
+++ plugin/talon_helpers/talon quit.py
@@ -0,0 +1,76 @@
+import os
+import subprocess
+import time
+from talon import Module, ui, actions, Context
+
+mod = Module()
+ctx_win = Context()
+ctx_mac = Context()
+
+ctx_win.matches = r"""
+os: windows
+"""
+
+ctx_mac.matches = r"""
+os: mac
+"""
+
+
+@mod.action_class
+class Actions:
+    def talon_restart():
+        """restart talon"""
+
+    def talon_quit():
+        """quit talon"""
+        talon_app = ui.apps(pid=os.getpid())[0]
+        print("Quitting:", talon_app)
+        talon_app.quit()
+
+    def restart_talon_with_confirmation():
+        """Check whether to really restart talon"""
+
+        def on_confirm():
+            actions.user.talon_restart()
+
+        def on_cancel():
+            pass
+
+        actions.user.are_you_sure_set_on_confirmation_action(
+            "Really restart talon?", on_confirm, on_cancel
+        )
+
+    def quit_talon_with_confirmation():
+        """Check whether to really quit talon"""
+
+        def on_confirm():
+            actions.user.talon_quit()
+
+        def on_cancel():
+            pass
+
+        actions.user.are_you_sure_set_on_confirmation_action(
+            "Really quit talon?", on_confirm, on_cancel
+        )
+
+
+@ctx_win.action_class("user")
+class Actions:
+    def talon_restart():
+        """restart talon"""
+        talon_app = ui.apps(pid=os.getpid())[0]
+        print("Restarting:", talon_app)
+        os.startfile(talon_app.exe)
+        talon_app.quit()
+
+
+@ctx_mac.action_class("user")
+class Actions:
+    def talon_restart():
+        """restart talon"""
+        talon_app = ui.apps(pid=os.getpid())[0]
+        print("Restarting:", talon_app)
+        talon_path = talon_app.path
+        print("Path to app:", talon_path)
+        subprocess.Popen(f"sleep 2; open {talon_path}", shell=True)
+        talon_app.quit()
---
+++ plugin/talon_helpers/talon_quit.talon
@@ -0,0 +1,2 @@
+talon restart: user.restart_talon_with_confirmation()
+talon quit: user.quit_talon_with_confirmation()
